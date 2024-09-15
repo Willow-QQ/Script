250453dd3a6141eda379b65f62a3400a641a9eec
Willow-QQ <202268568@qq.com>
2024-09-16 01:35:51 +0800

    更新 dlcs.js

--- dlcs.js
+++ dlcs.js
@@ -5,34 +5,31 @@
 [mitm]
 hostname = m.kuaidi100.com
 *************************************/
-// 模拟的原始 JSON 数据
+// 原始 JSON 数据
 let jsonData = {
-    "result": true,
-    "message": "成功",
-    "status": "200",
-    "count": 0,
-    "data": {
-        "mobile": "13285818801",
-        "vipTag": "NORMAL",
-        "vipDeadline": 1723775698804,
-        "workerPosition": "MANAGER",
-        "headImg": null,
-        "name": "店长",
-        "mktName": "千姿秀"
-    },
-    "total": 0
+  "result" : true,
+  "message" : "成功",
+  "status" : "200",
+  "count" : 0,
+  "data" : {
+    "mobile" : "13285818801",
+    "vipTag" : "NORMAL",
+    "vipDeadline" : 1723775698804,
+    "workerPosition" : "MANAGER",
+    "headImg" : null,
+    "name" : "店长",
+    "mktName" : "千姿秀"
+  },
+  "total" : 0
 };
 
-// 将 VIP 标签更改为 VIP
+// 将 vipTag 改为 VIP
 jsonData.data.vipTag = "VIP";
 
-// 设置一个未来的时间戳（这里设置为当前时间往后 3600000 毫秒，即 1 小时）
+// 设置 vipDeadline 为 1 年后的时间
 let now = new Date();
-let newDeadline = new Date(now.getTime() + 3600000);
+let newDeadline = new Date(now.getTime() + 365 * 24 * 60 * 60 * 1000); // 1年 = 365天
 jsonData.data.vipDeadline = newDeadline.getTime();
 
 // 输出修改后的 JSON 数据
-console.log(jsonData);
-
-
-
+console.log(JSON.stringify(jsonData, null, 2));
